03216f955ecdd20b98f3102f49ae847a34c1274a
Adam Bouhenguel <adam@bouhenguel.com>
2023-10-19 11:13:22 -0400

    visualizer: make it a lens that viz at /
    fix svg scaling, switch to sync enumeration

--- services/visualizer/src/process-dir.js
+++ services/visualizer/src/process-dir.js
@@ -3,15 +3,15 @@ import * as nodePath from 'path';
 import { shouldExcludePath } from './should-exclude-path';
 
 
-export const processDir = async (rootPath = "", excludedPaths = [], excludedGlobs = []) => {
+export const processDir = (rootPath = "", excludedPaths = [], excludedGlobs = []) => {
   const foldersToIgnore = [".git", ...excludedPaths]
   const fullPathFoldersToIgnore = new Set(foldersToIgnore.map((d) =>
     nodePath.join(rootPath, d)
   ));
 
 
-  const getFileStats = async (path = "") => {
-    const stats = await fs.statSync(`./${path}`);
+  const getFileStats = (path = "") => {
+    const stats = fs.statSync(`./${path}`);
     const name = path.split("/").filter(Boolean).slice(-1)[0];
     const size = stats.size;
     const relativePath = path.slice(rootPath.length + 1);
@@ -21,7 +21,7 @@ export const processDir = async (rootPath = "", excludedPaths = [], excludedGlob
       size,
     };
   };
-  const addItemToTree = async (
+  const addItemToTree = (
     path = "",
     isFolder = true,
   ) => {
@@ -29,7 +29,7 @@ export const processDir = async (rootPath = "", excludedPaths = [], excludedGlob
       console.log("Looking in ", `./${path}`);
 
       if (isFolder) {
-        const filesOrFolders = await fs.readdirSync(`./${path}`);
+        const filesOrFolders = fs.readdirSync(`./${path}`);
         const children = [];
 
         for (const fileOrFolder of filesOrFolders) {
@@ -39,14 +39,14 @@ export const processDir = async (rootPath = "", excludedPaths = [], excludedGlob
           }
 
           const info = fs.statSync(`./${fullPath}`);
-          const stats = await addItemToTree(
+          const stats = addItemToTree(
             fullPath,
             info.isDirectory(),
           );
           if (stats) children.push(stats);
         }
 
-        const stats = await getFileStats(path);
+        const stats = getFileStats(path);
         return { ...stats, children };
       }
 
@@ -62,7 +62,7 @@ export const processDir = async (rootPath = "", excludedPaths = [], excludedGlob
     }
   };
 
-  const tree = await addItemToTree(rootPath);
+  const tree = addItemToTree(rootPath);
 
   return tree;
 };
