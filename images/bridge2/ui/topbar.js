81455d49fcfdf4aa2035ff890a47ed5c67216f74
Matt Good <29016+mgood@users.noreply.github.com>
2024-02-08 11:43:30 -0800

    bridge2: whitespace cleanup
    Re-save UI files to remove some extra whitespace.
    
    Includes a minimal `.prettierrc.toml` as a start
    for #112. Though currently `requirePragma`
    disables the formatter by default unless files
    have a special comment to show that they've
    already been formatted. I'll test out the settings
    a bit to see how it looks, and then can
    incrementally format the files as we go.

--- images/bridge2/ui/topbar.js
+++ images/bridge2/ui/topbar.js
@@ -1,18 +1,17 @@
-
 export var Topbar = {
   view: ({attrs}) => {
     const localMedia = attrs.localMedia;
     return m("div", {"class":"flex flex-wrap px-6 py-4","id":"topbar"},
       [
-        m("h1", {"class":"py-1 text-xl font-bold"}, 
+        m("h1", {"class":"py-1 text-xl font-bold"},
           "session1"
         ),
-        m("div", {"class":"grow"}, 
+        m("div", {"class":"grow"},
           m.trust("&nbsp;")
         ),
         m("div", {"class":"flex space-x-2","id":"mic-controls"}, [
 
-          m("select", {name:"mic", id: "mic", 
+          m("select", {name:"mic", id: "mic",
             class: "py-2 px-3 pr-9 border border-gray-600 rounded-md text-md focus:border-blue-500 focus:ring-blue-500 bg-gray-700 text-gray-400",
             onchange: (e) => localMedia.setAudioSource(e.target.value),
             disabled: false
@@ -21,7 +20,7 @@ export var Topbar = {
               return m("option", {value: device.deviceId}, device.label)
             })
           ),
-    
+
           m("button", {name:"mic-mute", id:"mic-mute",
             class:"p-2 border border-gray-600 rounded-md text-md focus:border-blue-500 focus:ring-blue-500 bg-gray-700 text-gray-400",
             disabled: false,
@@ -29,7 +28,7 @@ export var Topbar = {
           },
             localMedia.audioEnabled ? "Mute" : "Unmute"
           ),
-    
+
           m("button", {
             class:"p-2 border border-gray-600 rounded-md text-md focus:border-blue-500 focus:ring-blue-500 bg-gray-700 text-gray-400",
             disabled: false,
@@ -37,7 +36,7 @@ export var Topbar = {
           },
             "Send System Audio"
           )
-    
+
         ])
       ]
     )
