e61d7b09658714e42303b4e4f182cf6f376da8a6
Matt Good <29016+mgood@users.noreply.github.com>
2024-02-09 09:40:37 -0800

    bridge2: fixes for setting audio-only
    Use `videoSource = false` instead to disable
    capturing video. The `videoEnabled` flag should
    control toggling it on and off during a session
    instead.
    
    Also updates some places that assume there's
    always at least one video and audio track to
    account for when they're empty.

--- images/bridge2/webrtc/js/localmedia.js
+++ images/bridge2/webrtc/js/localmedia.js
@@ -57,16 +57,26 @@ class LocalMedia {
         systemAudio: 'include',
       });
     } else {
+      const source = (src) => {
+        if (src === false) {
+          return false;
+        }
+        return {deviceId: src ? {exact: src} : true};
+      }
       this.stream = await navigator.mediaDevices.getUserMedia({
-        audio: this.audioEnabled ? {deviceId: this.audioSource ? {exact: this.audioSource} : true} : false,
-        video: this.videoEnabled ? {deviceId: this.videoSource ? {exact: this.videoSource} : true} : false,
+        audio: source(this.audioSource),
+        video: source(this.videoSource),
       });
     }
     if (!this.audioEnabled) {
-      this.stream.getAudioTracks()[0].enabled = false;
+      for (const track of this.stream.getAudioTracks()) {
+        track.enabled = false;
+      }
     }
     if (!this.videoEnabled) {
-      this.stream.getVideoTracks()[0].enabled = false;
+      for (const track of this.stream.getVideoTracks()) {
+        track.enabled = false;
+      }
     }
     if (this.onstreamchange) {
       this.onstreamchange(this.stream);
