6fe9f59bb04c4bae06d61143a5cd77240bf2fb59
Matt Good <29016+mgood@users.noreply.github.com>
2024-07-25 11:35:48 -0700

    dynamic JS client commands
    Adds classes to implement static & dynamic commands.
    This changes `index` and `run` to be async so that
    they can dispatch a request for dynamic commands.

--- images/bridge2/ui/substrate-r0.js
+++ images/bridge2/ui/substrate-r0.js
@@ -1,18 +1,46 @@
+export class StaticCommands {
+    constructor(commands) {
+        const index = {}, runners = {}
+        for (const [name, {parameters, returns, run}] of Object.entries(commands)) {
+            runners[name] = (parameters) => run(parameters) // TODO just set = run directly?
+            index[name] = {parameters, returns}
+        }
+        this.index = Promise.resolve(index)
+        this.runners = runners
+    }
+    async run(command, parameters) {
+        return await this.runners[command](parameters)
+    }
+}
+
+export class ReflectCommands {
+    constructor(url) {
+        this.url = url || window.location.href
+    }
+
+    get index() {
+        return fetch(this.url, { method: "REFLECT" }).then(resp => resp.json());
+    }
+
+    async run(command, parameters) {
+        return await fetch(this.url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ command, parameters }),
+        }).then(resp => resp.json());
+    }
+}
+
+// TODO chaining to combine static & dynamic commands, or multiples of each
+
 window.substrate = {
     r0: {
         commands: {
-            index: {},
-            runners: {},
-            run(command, parameters) { return this.runners[command](parameters) },
+            index: Promise.resolve({}),
+            async run(command, parameters) { throw new Error("No commands available") },
         },
         setCommands(commands) {
-            const index = {}, runners = {}
-            for (const [name, {parameters, returns, run}] of Object.entries(commands)) {
-                runners[name] = (parameters) => run(parameters)
-                index[name] = {parameters, returns}
-            }
-            this.commands.index = index
-            this.commands.runners = runners
+            this.commands = new StaticCommands(commands)
         },
     }
 }
