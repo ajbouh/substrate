4df5029181a16c6caa9fd9dfbe39e478b1fdd030
Matt Good <29016+mgood@users.noreply.github.com>
2024-08-01 10:14:24 -0700

    add command-panel source

---
+++ pkg/command-panel/src/lib/CommandPanel.svelte
@@ -0,0 +1,76 @@
+<!-- <svelte:options customElement="command-panel" /> -->
+
+<script lang="ts">
+	import type { Commands, Def, DefIndex } from '$lib/defs.ts';
+	import Command from './Command.svelte';
+	let { commands = null }: { commands: Commands | null } = $props();
+	async function run(name: string, props: Record<string, any>) {
+		if (!commands) return;
+		// TODO append to a log of the commands & returns
+		let ret = await commands.run(name, props);
+		console.log('ran', name, props, ret);
+	}
+	let index = $derived(commands?.index || new Promise<DefIndex>(() => {}));
+	let search = $state('');
+	function filter(key: string, def: Def) {
+		if (!search) return true;
+		let lower = search.toLowerCase();
+		return key.toLowerCase().includes(lower) || def.description.toLowerCase().includes(lower);
+	}
+	let focused = $state(false);
+	let open = $derived(focused || search);
+</script>
+
+<div class="command-panel" class:open>
+	<div class="search">
+		<input
+			type="search"
+			bind:value={search}
+			placeholder="Search commands..."
+			onfocus={() => {
+				focused = true;
+			}}
+			onblur={() => {
+				focused = false;
+			}}
+		/>
+	</div>
+
+	<div class="commands">
+		{#await index}
+			<p>Loading...</p>
+		{:then index}
+			{#each Object.keys(index) as key}
+				{#if filter(key, index[key])}
+					<Command name={key} def={index[key]} run={(props) => run(key, props)} />
+				{/if}
+			{/each}
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	</div>
+</div>
+
+<style>
+	.command-panel {
+		background: white;
+		color: black;
+
+		display: flex;
+		flex-direction: column;
+		position: fixed;
+		bottom: 0;
+		padding: 1em;
+		overflow: auto;
+	}
+	.command-panel.open {
+		height: 80%;
+		width: 80%;
+	}
+	.command-panel.open .commands {
+		display: block;
+	}
+	.commands {
+		display: none;
+	}
+</style>
