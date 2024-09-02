7b054a832fd82a6d151306ef956ac01a3b54433a
Adam Bouhenguel <adam@bouhenguel.com>
2024-09-02 18:04:04 -0400

    feat: add cue eval command

--- images/substrate/http/defs.go
+++ images/substrate/http/defs.go
@@ -2,12 +2,9 @@ package substratehttp
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 
-	"cuelang.org/go/cue"
 	"github.com/ajbouh/substrate/images/substrate/defset"
-	"github.com/ajbouh/substrate/images/substrate/httputil"
 	"github.com/ajbouh/substrate/pkg/toolkit/httpevents"
 	"github.com/ajbouh/substrate/pkg/toolkit/notify"
 )
@@ -20,31 +17,4 @@ type DefsHandler struct {
 
 func (h *DefsHandler) ContributeHTTP(mux *http.ServeMux) {
 	mux.Handle(fmt.Sprintf("GET %s/v1/defs", h.Prefix), h.DefsAnnouncer)
-	mux.Handle(fmt.Sprintf("POST %s/v1/eval", h.Prefix), http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		defset := h.DefSetLoader.Load()
-
-		defer req.Body.Close()
-		b, err := io.ReadAll(req.Body)
-		if err != nil {
-			http.Error(rw, fmt.Sprintf(`{"message": %q}`, err.Error()), http.StatusInternalServerError)
-			return
-		}
-
-		var status int
-		var resp []byte
-		func() {
-			defset.CueMu.Lock()
-			defer defset.CueMu.Unlock()
-
-			// HACK just trying to make an empty struct
-			scope := defset.CueContext.CompileString("{...}", cue.ImportPath("_"))
-			scope = scope.FillPath(cue.MakePath(cue.Hid("_root", "_")), defset.RootValue)
-
-			result := defset.CueContext.CompileBytes(b, cue.ImportPath("_"), cue.Scope(scope))
-
-			status, resp = httputil.WriteCueValue(req, result)
-		}()
-		rw.WriteHeader(status)
-		rw.Write(resp)
-	}))
 }
