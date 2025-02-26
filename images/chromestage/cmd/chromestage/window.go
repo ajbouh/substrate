0cfa561f6c8ad5de94369e874cade22c8e831eb2
Adam Bouhenguel <adam@bouhenguel.com>
2025-02-26 15:17:28 -0500

    feat: add rasterize to chromestage, rm tab: prefix
    properly wait for chromedp client during chromestage reflect

--- images/chromestage/cmd/chromestage/window.go
+++ images/chromestage/cmd/chromestage/window.go
@@ -5,30 +5,25 @@ import (
 	"fmt"
 	"os/exec"
 
-	"github.com/ajbouh/substrate/pkg/toolkit/commands"
 	"github.com/ajbouh/substrate/pkg/toolkit/commands/handle"
 )
 
-func WindowCommands() commands.Source {
-	return commands.List(
-		handle.Command(
-			"resize",
-			"Resize the window to the given `width` and `height`",
-			func(ctx context.Context, t *struct{}, args struct {
-				Width  int64 `json:"width"`
-				Height int64 `json:"height"`
-			}) (struct{}, error) {
-				if err := exec.Command("xrandr", "-s", fmt.Sprintf("%dx%d", args.Width, args.Height)).Run(); err != nil {
-					return struct{}{}, err
-				}
-				if err := exec.Command(
-					"xdotool", "getwindowfocus",
-					"windowsize", fmt.Sprintf("%d", args.Width), fmt.Sprintf("%d", args.Height),
-				).Run(); err != nil {
-					return struct{}{}, err
-				}
-				return struct{}{}, nil
-			},
-		),
-	)
-}
+var WindowResize = handle.Command(
+	"window:resize",
+	"Resize the window to the given `width` and `height`",
+	func(ctx context.Context, t *struct{}, args struct {
+		Width  int64 `json:"width"`
+		Height int64 `json:"height"`
+	}) (struct{}, error) {
+		if err := exec.Command("xrandr", "-s", fmt.Sprintf("%dx%d", args.Width, args.Height)).Run(); err != nil {
+			return struct{}{}, err
+		}
+		if err := exec.Command(
+			"xdotool", "getwindowfocus",
+			"windowsize", fmt.Sprintf("%d", args.Width), fmt.Sprintf("%d", args.Height),
+		).Run(); err != nil {
+			return struct{}{}, err
+		}
+		return struct{}{}, nil
+	},
+)
