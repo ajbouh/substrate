c5796c376d778f39437f9dfecfad724c39bf12ac
Adam Bouhenguel <adam@bouhenguel.com>
2025-03-03 12:56:13 -0500

    chore: reduce fs round trips in /esbuild/

--- images/spaceview/units/esbuild.go
+++ images/spaceview/units/esbuild.go
@@ -10,6 +10,7 @@ import (
 	"path"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/evanw/esbuild/pkg/api"
 
@@ -20,11 +21,11 @@ type EsbuildRoute struct {
 	BaseDir string
 }
 
-var MinifyPattern = regexp.MustCompile("\\.min(\\.[^\\.]+)$")
+var CompilePattern = regexp.MustCompile("(\\.ts)$")
 
-func exists(p string) bool {
-	_, err := os.Stat(p)
-	return err == nil
+func exists(p string) os.FileInfo {
+	stat, _ := os.Stat(p)
+	return stat
 }
 
 func readBuildOptions(opt *api.BuildOptions, path string) (bool, error) {
@@ -40,62 +41,77 @@ func readBuildOptions(opt *api.BuildOptions, path string) (bool, error) {
 	return true, json.NewDecoder(f).Decode(opt)
 }
 
-func (d *EsbuildRoute) detectBuildOptions(file string) (*api.BuildOptions, error) {
+func (d *EsbuildRoute) detectBuildOptions(file string) (bool, time.Time, *api.BuildOptions, error) {
 	opt := &api.BuildOptions{}
 	file = strings.TrimSuffix(file, "/")
-	switch {
-	case exists(d.BaseDir + "/" + file + "/esbuild.json"):
-		opt.AbsWorkingDir = d.BaseDir + "/" + file
-		opt.Outfile = path.Base(file)
-		present, err := readBuildOptions(opt, d.BaseDir+"/"+file+"/esbuild.json")
-		if present {
-			return opt, err
-		}
-	case exists(d.BaseDir + "/" + file + ".esbuild.json"):
-		opt.AbsWorkingDir = d.BaseDir + "/" + path.Dir(file)
-		opt.Outfile = path.Base(file)
-		present, err := readBuildOptions(opt, d.BaseDir+"/"+file+".esbuild.json")
-		if present {
-			return opt, err
-		}
-	case exists(d.BaseDir + "/" + file):
-		opt.AbsWorkingDir = d.BaseDir
-		opt.Outfile = file
-		if MinifyPattern.MatchString(file) {
-			opt.MinifyWhitespace = true
-			opt.MinifyIdentifiers = true
-			opt.MinifySyntax = true
-			suffix := MinifyPattern.FindStringSubmatch(file)[1]
-			opt.EntryPoints = []string{strings.TrimSuffix(file, ".min"+suffix) + suffix}
-		} else {
-			opt.EntryPoints = []string{file}
+
+	if stat := exists(d.BaseDir + "/" + file); stat != nil {
+		if !CompilePattern.MatchString(file) {
+			return false, stat.ModTime(), nil, nil
 		}
 
 		opt.AbsWorkingDir = d.BaseDir
-		opt.Bundle = true
-		opt.Format = api.FormatDefault
+		opt.Outfile = file
+		opt.EntryPoints = []string{file}
+
+		opt.Format = api.FormatESModule // api.FormatDefault
 		opt.Platform = api.PlatformBrowser
 		opt.LogLevel = api.LogLevelSilent
 
-		switch {
-		case strings.HasSuffix(file, ".cjs"):
-			opt.Format = api.FormatCommonJS
-		case strings.HasSuffix(file, ".mjs"):
-			opt.Format = api.FormatESModule
-		}
-		return opt, nil
+		return true, stat.ModTime(), opt, nil
 	}
 
-	return nil, nil
+	if present, err := readBuildOptions(opt, d.BaseDir+"/"+file+"/esbuild.json"); present {
+		opt.AbsWorkingDir = d.BaseDir + "/" + file
+		opt.Outfile = path.Base(file)
+		if present {
+			return true, time.Time{}, opt, err
+		}
+	}
+
+	if present, err := readBuildOptions(opt, d.BaseDir+"/"+file+".esbuild.json"); present {
+		opt.AbsWorkingDir = d.BaseDir + "/" + path.Dir(file)
+		opt.Outfile = path.Base(file)
+		if present {
+			return true, time.Time{}, opt, err
+		}
+	}
+
+	return false, time.Time{}, nil, nil
 }
 
 func (d *EsbuildRoute) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	file := r.PathValue("file")
 
-	opt, err := d.detectBuildOptions(file)
+	doBuild, mtime, opt, err := d.detectBuildOptions(file)
 	if err != nil {
 		http.Error(w, fmt.Sprintf(`{"message": %q}`, err.Error()), http.StatusInternalServerError)
 		return
+	}
+
+	if !doBuild {
+		routePrefix := strings.TrimSuffix(r.URL.Path, file)
+		filePrefix := d.BaseDir + "/"
+		http.StripPrefix(routePrefix, http.FileServerFS(os.DirFS(filePrefix))).ServeHTTP(w, r)
+		return
+	}
+
+	// optionally skip the build if we know the browser has the same or newer. this might be a
+	// source of bugs at some point, but at the moment we're trying to keep the number of
+	// I/O operations to keep latency down. this is because we might be reading files over a network
+	// file system.
+	if !mtime.IsZero() {
+		if ifModifiedSince := r.Header.Get("If-Modified-Since"); r.Method == http.MethodGet && ifModifiedSince != "" {
+			parsedTime, err := time.Parse(http.TimeFormat, ifModifiedSince)
+			if err == nil {
+				if !mtime.Truncate(time.Second).After(parsedTime) {
+					h := w.Header()
+					h.Set("Cache-Control", "max-age=0")
+					w.WriteHeader(http.StatusNotModified)
+					return
+				}
+			}
+		}
 	}
 
 	if opt != nil {
@@ -122,6 +138,11 @@ func (d *EsbuildRoute) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			if trimmed == opt.Outfile {
 				h := w.Header()
 				h.Set("Content-Type", "text/javascript")
+				if !mtime.IsZero() {
+					// expect the browser to issue conditional gets
+					h.Set("Cache-Control", "max-age=0")
+					h.Set("Last-Modified", mtime.Format(http.TimeFormat))
+				}
 				w.Write(f.Contents)
 				return
 			}
